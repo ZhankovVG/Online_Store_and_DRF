55ae70fde8a4c11a31f4a0d57c3edb514efeb752
Vitaliy Zhankov <sprinter-100m@ukr.net>
2023-07-04 18:27:14 +0300

    start project

---
+++ Online_Store/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.decorators.cache import never_cache
+from django.contrib.staticfiles.views import serve
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Shop.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
