3b3e8edc9e355aaa54db4c7edb59c8087aa8e5ab
Vitaliy Zhankov <sprinter-100m@ukr.net>
2023-07-05 17:37:24 +0300

    adding a master page

--- Shop/urls.py
+++ Shop/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
+    path('', views.ProductView.as_view(), name='home')
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+class ProductView(ListView):
+    # Output Produckt
+    model = Product
+    queryset = Product.objects.filter(available=False)
