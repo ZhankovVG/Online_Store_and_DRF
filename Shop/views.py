66be4bd8b10d9c5d868f8a26b4f32536f26533ee
Vitaliy Zhankov <sprinter-100m@ukr.net>
2023-07-06 19:44:50 +0300

    adding html and css

--- Shop/views.py
+++ Shop/views.py
@@ -7,3 +7,8 @@ class ProductView(ListView):
     # Output Produckt
     model = Product
     queryset = Product.objects.filter(available=False)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        return context
