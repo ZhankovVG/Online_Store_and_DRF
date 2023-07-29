c3be140a6877acb015f78f264cfbb71cf97f40ad
vitaliy <vitaliy@Ubuntu.myguest.virtualbox.org>
2023-07-29 16:59:17 +0300

    adding postgreSQL and correct error

--- Online_Store/settings.py
+++ Online_Store/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'Online_Store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'Create_PostgreSQL_1',                     
+        'USER': 'admin',                     
+        'PASSWORD': '1080Zxc',             
+        'HOST': 'localhost',                        
+        'PORT': '5432',                                 
     }
 }
 
