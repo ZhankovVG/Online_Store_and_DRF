b23347df0710afad653bccde5bd09a1daada3a1f
vitaliy <vitaliy@Ubuntu.myguest.virtualbox.org>
2023-08-05 18:19:00 +0300

    some minor edits have been made

--- Online_Store/settings.py
+++ Online_Store/settings.py
@@ -169,7 +169,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
